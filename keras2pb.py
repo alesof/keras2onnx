51819235da064dcc4e2cde4c300ec67f7c41d52a
Alessandro Sofia <al.sofia@studenti.unina.it>
2023-11-30 19:55:01 +0100

    Clean and format
    Add param from command line
    Using functions for modularity
    Using logging instead of print

--- keras2pb.py
+++ keras2pb.py
@@ -1,22 +1,41 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import load_model
 import os
+import logging
+import argparse
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
-verbose = False
-dir_name = 'models'
-model_name = 'leather_suede.h5'
-output_dir_name = 'pb_model'
+def load_and_summary_model(model_path, verbose=False):
+    model = load_model(model_path)
 
-full_model_path = os.path.join(dir_name, model_name)
-output_path = os.path.join(dir_name, output_dir_name)
+    if verbose:
+        logging.info("\nTensorFlow version: %s", tf.__version__)
+        logging.info("Keras version: %s\n", tf.keras.__version__)
+        logging.info("Model path: %s\n", model_path)
 
-model = load_model(full_model_path)
+    model.summary()
+    return model
 
-if verbose:
-    print("\nTensorFlow version:", tf.__version__)
-    print("Keras version:", keras.__version__, "\n")
-    print("Model path: ", model_path, "\n")
+def save_model_as_tf(model, output_path):
+    model.save(filepath=output_path, save_format='tf')
 
-model.summary()
-model.save(filepath=output_path, save_format='tf')
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Load TF model.")
+    parser.add_argument("model_path", type=str, help="Path to the model file.")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose.")
+    return parser.parse_args()
+
+def main():
+
+    args = parse_arguments()
+
+    full_model_path = args.model_path
+    output_path = os.path.join(os.path.dirname(full_model_path), 'pb_model')
+
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+    model = load_and_summary_model(full_model_path, args.verbose)
+
+    save_model_as_tf(model, output_path)
+
+if __name__ == "__main__":
+    main()
